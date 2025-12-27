e775e1489ae06e0a3a70d286a9af94743197badd
Chris Lesiw <chris@chrislesiw.com>
2025-12-27 16:28:25 -0500

    Remove unused ReadFileFS interface
    This should be a convenience wrapper for reading files,
    not its own unique capability.

--- readfile.go
+++ readfile.go
@@ -2,37 +2,14 @@ package fs
 
 import (
 	"context"
-	"errors"
 	"io"
 )
-
-// A ReadFileFS is a file system with the ReadFile method.
-//
-// If not implemented, ReadFile falls back to Open and [io.ReadAll].
-type ReadFileFS interface {
-	FS
-
-	// ReadFile reads the named file and returns its contents.
-	ReadFile(ctx context.Context, name string) ([]byte, error)
-}
 
 // ReadFile reads the named file and returns its contents.
 // Analogous to: [io/fs.ReadFile], [os.ReadFile], cat.
 //
-// Requires: [ReadFileFS] || [FS]
+// Requires: [FS]
 func ReadFile(ctx context.Context, fsys FS, name string) ([]byte, error) {
-	if rffs, ok := fsys.(ReadFileFS); ok {
-		data, err := rffs.ReadFile(ctx, name)
-		if err != nil && !errors.Is(err, ErrUnsupported) {
-			return data, err
-		}
-		if err == nil {
-			return data, nil
-		}
-		// Fall through to fallback if ErrUnsupported
-	}
-	// Fall back to opening and reading
-	// (always possible - uses base FS interface)
 	f, err := Open(ctx, fsys, name)
 	if err != nil {
 		return nil, err
